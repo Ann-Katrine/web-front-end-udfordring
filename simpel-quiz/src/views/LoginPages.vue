1c4fba55f78697c520fe1926c24f1019cded6d39
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2023-01-12 18:31:59 +0100

    simpel login side
    Jeg har lavet en simpel login side uden noget css eller sesstion token

---
+++ simpel-quiz/src/views/LoginPages.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div>
+      <label>Email: </label><input type="email" v-model="email"/>
+    </div>
+    <div>
+      <label>Password</label><input type="password" v-model="password"/>
+    </div>
+    <button @click="login()">Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'LoginPages',
+  data(){
+    return{
+      email: "",
+      password: ""
+    }
+  },
+  methods:{
+    login(){
+      if(this.email == "hej@" && this.password == '123'){
+        this.$router.push("/quiz")
+      }
+    }
+  }
+});
+</script>
